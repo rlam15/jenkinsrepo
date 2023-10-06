77a27cb0a30b5d940e5e4e50747bd88a944ff6b7
Randy Lam <rlam27@ucmerced.edu>
2023-10-06 13:21:27 -0700

    anotehr test

--- jenkins.groovy
+++ jenkins.groovy
@@ -12,10 +12,11 @@ pipeline {
         stage('Running Autobuild.py') {
             steps {
                 // Always use back slash instead of foward slash to access specific repos.
-                dir("c:/Users/RLAM8/Documents/Shield Docker/shield-docker-build-master/") {
-                    // powershell ('docker shield-build -t .')
-                    powershell ('python.exe auto-build.py --force') 
-                    echo ('Finished Building!')
+                // dir("c:/Users/RLAM8/Documents/Shield Docker/shield-docker-build-master/") {
+                //     // powershell ('docker shield-build -t .')
+                //     powershell ('python.exe auto-build.py --force') 
+                //     echo ('Finished Building!')
+                powershell ('pwd')
                 }
             }
         }
